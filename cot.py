ddd5db0bca20344464e851529484ebbf1069787c
João Lucas <jhoaw@proton.me>
2024-04-17 23:58:52 -0300

    First commit

--- cot.py
+++ cot.py
@@ -5,42 +5,71 @@ def get_url(url):
     try:
         import requests
     except ModuleNotFoundError:
-        print('Erro: o módulo "requests" não está instalado.')
+        print('\nErro: o módulo "requests" não está instalado.'
+        '\nInstale o pacote "requests" com o pip!\n')
         exit(1)
     try:
         # Faz a requisição a API do BC e guarda o conteúdo em uma variável
         response = requests.get(url)
         data = response.text
     except Exception as error:
-        print(f'Erro: {error}')
+        print(f'\nErro: {error}')
     else:
         return data
 
-def fetch_current_conversion(content):
+def fetch_current_conversion(content, verbose=False):
     """Filtra a tag <taxaVenda> do XML resgatado e retorna o valor dela."""
     try:
         from bs4 import BeautifulSoup, FeatureNotFound
         # Inicia o módulo BeautifulSoup
         soup = BeautifulSoup(content, 'xml')
     except ModuleNotFoundError:
-        print('Erro! o módulo "BeautifulSoup" não está instalado.')
+        print('\nErro! o módulo "BeautifulSoup" não está instalado.\n'
+              'Instale o pacote "bs4" com o pip!\n')
         exit(1)
     except FeatureNotFound:
-        print('Erro! Você não possuí um interpretador de XML válido instalado!')
+        print('\nErro! Você não possuí um interpretador de XML válido instalado!'
+              'Instale o pacote "lxml" com o pip!\n')
+        exit(1)
     else:
-        # Filtra o arquivo XML para puxar o valor de venda do dolár em real
-        conversion = soup.find(name='taxaVenda')
-        return conversion
+        # Filtra o arquivo XML para puxar o valor de venda do dolár em result
+        if verbose:
+            extra_info = []
+            seller_tax = soup.find(name='taxaVenda')
+            buyer_tax = soup.find(name='taxaCompra')
+            is_from_buletin = soup.find(name='cotacaoBoletim')
+            extra_info.append(seller_tax)
+            extra_info.append(buyer_tax)
+            extra_info.append(is_from_buletin)
+            return extra_info
+        else:
+            conversion = soup.find(name='taxaVenda')
+            return conversion
 
-def display_conversion():
-    """Mostra o resultado formatado, com a data atual e valor do dólar em real."""
-    data_atual = datetime.now()
-    data_atual_formatada = data_atual.strftime('%Y-%m-%d')
 
-    api_url = f'https://www3.bcb.gov.br/bc_moeda/rest/cotacao/fechamento/ultima/1/220/{data_atual_formatada}'
+
+def display_conversion(url, verbose=False):
+    """Mostra o resultado formatado, com a data atual e valor do dólar em result."""
+    current_date = datetime.now()
+    current_date_formatted = current_date.strftime('%Y-%m-%d')
+
+    api_url = url + current_date_formatted
 
     data = get_url(api_url)
-    conversion = fetch_current_conversion(data)
 
-    print(f'Resultados de: {data_atual.strftime("%d/%m/%Y")}')
-    print(f'Valor de compra do dólar: R$ {conversion.text[:-6]}')
+    # Se o programa rodar como o argumento de verbosidade, mostrará mais informações sobre a atualização
+    if verbose:
+        conversion = fetch_current_conversion(data, verbose=True)
+        result = conversion[1].text[:-4]
+        print('-'*38)
+        print(f'Resultados de: {current_date.strftime("%d/%m/%Y")}'.center(38))
+        print('-'*38)
+        print(f'É uma cotação do boletim? -> {conversion[2].text}')
+        print(f'Valor de compra do dólar -> R$ {conversion[0].text[:-4]}'.replace('.', ','))
+        print(f'Valor de venda do dólar -> R$ {result}'.replace('.', ','))
+        print('-'*38)
+    else:
+        conversion = fetch_current_conversion(data)
+        result = conversion.text[:-6]
+        print(f'R$ {result}'.replace('.', ','))
+    return float(result)
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import argparse
 import cot
+# TODO Criar um módulo de formatação de moedas, para simplificar a função main abaixo
+# import formatmoney
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--real', help='Valor usado para converter de real para dólar', type=float)
+parser.add_argument('-d', '--dolar', help='Valor usado para converter de dólar para real', type=float)
+parser.add_argument('-v', '--verbose', help='Aumenta a verbose do programa', action='store_true')
+
+args = parser.parse_args()
+
+# URL da API atual do Banco do Brasil
+URL = f'https://www3.bcb.gov.br/bc_moeda/rest/cotacao/fechamento/ultima/1/220/'
 
 def main():
     try:
-        results = cot.display_conversion()
+        if args.verbose:
+            result = cot.display_conversion(url=URL, verbose=True)
+        else:
+            result = cot.display_conversion(URL)
+        if args.real:
+            print(f'\nR$ {args.real:.2f} -> $ 1.00 = $ {args.real // result:.2f}'.replace('.', ','))
+        if args.dolar:
+            print(f'$ {args.dolar:.2f} -> R$ 1.00 = R$ {result * args.dolar:.2f}'.replace('.', ','))
     except Exception as error:
         print(error)
 
